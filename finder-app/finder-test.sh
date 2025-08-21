3520d203f2549019db91785b35b2a5c31a64f85b
kienxoi <kienxoi@Ubuntu.myguest.virtualbox.org>
2025-08-21 17:35:06 +0700

    update

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -2,6 +2,7 @@
 # Tester script for assignment 1 and assignment 2
 # Author: Siddhant Jajoo
 # Modified for Assignment 2 to use C writer instead of shell script
+# Modified for Assignment 3 Part 1: remove make step
 
 set -e
 set -u
@@ -13,17 +14,17 @@ username=$(cat conf/username.txt)
 
 if [ $# -lt 3 ]
 then
-	echo "Using default value ${WRITESTR} for string to write"
-	if [ $# -lt 1 ]
-	then
-		echo "Using default value ${NUMFILES} for number of files to write"
-	else
-		NUMFILES=$1
-	fi	
+    echo "Using default value ${WRITESTR} for string to write"
+    if [ $# -lt 1 ]
+    then
+        echo "Using default value ${NUMFILES} for number of files to write"
+    else
+        NUMFILES=$1
+    fi    
 else
-	NUMFILES=$1
-	WRITESTR=$2
-	WRITEDIR=/tmp/aeld-data/$3
+    NUMFILES=$1
+    WRITESTR=$2
+    WRITEDIR=/tmp/aeld-data/$3
 fi
 
 MATCHSTR="The number of files are ${NUMFILES} and the number of matching lines are ${NUMFILES}"
@@ -33,29 +34,29 @@ echo "Writing ${NUMFILES} files containing string ${WRITESTR} to ${WRITEDIR}"
 rm -rf "${WRITEDIR}"
 
 # create $WRITEDIR if not assignment1
-assignment=`cat ../conf/assignment.txt`
+assignment=$(cat ../conf/assignment.txt)
 
 if [ $assignment != 'assignment1' ]
 then
-	mkdir -p "$WRITEDIR"
+    mkdir -p "$WRITEDIR"
 
-	if [ -d "$WRITEDIR" ]
-	then
-		echo "$WRITEDIR created"
-	else
-		exit 1
-	fi
+    if [ -d "$WRITEDIR" ]
+    then
+        echo "$WRITEDIR created"
+    else
+        exit 1
+    fi
 fi
 
-# --- New: Build writer from C source ---
-echo "Removing the old writer utility and compiling as a native application"
-make clean
-make
+# --- Removed make step for Assignment 3 Part 1 ---
+# echo "Removing the old writer utility and compiling as a native application"
+# make clean
+# make
 
-# --- Replace writer.sh with writer (binary) ---
-for i in $( seq 1 $NUMFILES)
+# --- Use writer binary directly ---
+for i in $( seq 1 $NUMFILES )
 do
-	./writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
+    ./writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
 done
 
 OUTPUTSTRING=$(./finder.sh "$WRITEDIR" "$WRITESTR")
@@ -66,10 +67,10 @@ rm -rf /tmp/aeld-data
 set +e
 echo ${OUTPUTSTRING} | grep "${MATCHSTR}"
 if [ $? -eq 0 ]; then
-	echo "success"
-	exit 0
+    echo "success"
+    exit 0
 else
-	echo "failed: expected  ${MATCHSTR} in ${OUTPUTSTRING} but instead found"
-	exit 1
+    echo "failed: expected  ${MATCHSTR} in ${OUTPUTSTRING} but instead found"
+    exit 1
 fi
 
